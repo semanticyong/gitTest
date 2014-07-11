9a2df5bdb4fb0256892fa4e27108dc48887bfcad
Yong <semanticyong@gmail.com>
2014-07-11 17:25:36 +0800

    new Proj

---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by yong58 on 7/11/14.
+//  Copyright (c) 2014 yong58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ gitTest/gitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by yong58 on 7/11/14.
+//  Copyright (c) 2014 yong58. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
